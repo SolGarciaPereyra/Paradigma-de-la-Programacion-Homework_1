08d6d109ba21b3ebf054ea3594839784839c6353
Sol Garcia Pereyra <sol.a.garciap@gmail.com>
2025-03-13 10:50:46 -0300

    Trabajando con mi funcion crear_mi_array

--- Ejercicio 1/ejercicio_1_matrices.cpp
+++ Ejercicio 1/ejercicio_1_matrices.cpp
@@ -3,5 +3,13 @@
 array<array<int, N>, N> crear_mi_matriz(){
     array<array<int, N>, N> mi_matriz;
 
+    int numerador = 1;
+    for(int position = 0; position < N; position++){
+        for(int index = 0; index < N; index++){
+            mi_matriz[position][index] = numerador;
+            numerador++;
+        }
+    }
 
+    return mi_matriz;
 }
