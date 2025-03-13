8ca5c2685b97a8b6cc94f54dcb34b0b592720e18
Sol Garcia Pereyra <sol.a.garciap@gmail.com>
2025-03-13 12:17:18 -0300

    Trabajando con mi funcion crear_mi_array

--- Ejercicio 1/main_ejercicio_1_matrices.cpp
+++ Ejercicio 1/main_ejercicio_1_matrices.cpp
@@ -1,5 +1,26 @@
 #include "ejercicio_1_matrices.hpp"
 
 int main(){
-    
+    array<array<int, N>, N> mi_matriz = crear_mi_matriz();
+
+    int contador = N - 1;
+    int seguimiento = N - 1;
+    int tracker = 0;
+    while(contador >=0){
+        if(tracker <= N){
+            if(seguimiento >= 0){
+                cout << "Matriz" << N << "[" << contador << "][" << seguimiento << "] = " << mi_matriz[contador][seguimiento] << endl; 
+                seguimiento--;  
+            }
+            else{
+                contador--;
+                seguimiento = N - 1;
+                tracker++;
+                continue;
+            }
+        }
+        else{
+            break;
+        }
+    }
 }
