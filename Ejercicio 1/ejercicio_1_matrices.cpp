cd3181edc9bb97e4afabede106c046f4c0140c18
Sol Garcia Pereyra <sol.a.garciap@gmail.com>
2025-03-13 00:22:49 -0300

    Actualice el nombre de mi funcion

--- Ejercicio 1/ejercicio_1_matrices.cpp
+++ Ejercicio 1/ejercicio_1_matrices.cpp
@@ -1,7 +1,7 @@
 #include "ejercicio_1_matrices.hpp"
 
-array<array<int, N>, N> crear_mi_array(){
-    array<array<int, N>, N> mi_array;
+array<array<int, N>, N> crear_mi_matriz(){
+    array<array<int, N>, N> mi_matriz;
 
 
 }
