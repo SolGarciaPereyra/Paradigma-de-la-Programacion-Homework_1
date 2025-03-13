d70f00101a19b2d91086dd38d73e75badb053af6
Sol Garcia Pereyra <sol.a.garciap@gmail.com>
2025-03-13 00:31:17 -0300

    Actualizando mi archivo .hpp: declaro mi funcion crear_mi_array y agrego un comentario

--- Ejercicio 1/ejercicio_1_matrices.hpp
+++ Ejercicio 1/ejercicio_1_matrices.hpp
@@ -2,4 +2,10 @@
 #include <array>
 using namespace std;
 
-const size_t N = 4;
+const int N = 3;  //Tamaño de mi matriz (lo elegi arbitrariamente)
+
+/*
+    Crea una matriz cuadrada, con valores que empiezan desde 1 hasta N^2, usando un valor entero positivo 
+    "N" (mayor a 1), el cual fue definido anteriormente.
+*/
+array<array<int, N>, N> crear_mi_matriz();
