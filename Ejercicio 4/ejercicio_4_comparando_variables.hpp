a768b98c1398278e058b15c4c26afb7d96009081
Sol Garcia Pereyra <sol.a.garciap@gmail.com>
2025-03-21 21:31:50 -0300

    Termine de realizar el inciso 4c y de justifcar el resultado obtenido. Actualizo mi repositorio en github.

--- Ejercicio 4/ejercicio_4_comparando_variables.hpp
+++ Ejercicio 4/ejercicio_4_comparando_variables.hpp
@@ -3,6 +3,12 @@
 #include <chrono>
 using namespace std;
 
+//Para el inciso 4c:
+//Defino mis variables con "constexpr" para que mi compilador conozca sus valores en tiempo de compilacion, y pueda entonces tambien realizar la funcion en tiempo de compilacion.
+constexpr const char variable1[] = "Se requiere el código de una función recursiva que compare dos variables que contengan texto e indique mediante una variable bool si son iguales.";
+constexpr const char variable2[] = "Se requiere el código de una función recursiva que compare dos variables que contengan texto e indique mediante una variable bool si son.";
+constexpr int index_2 = 0;   //Lo inicializo en ese valor ya que un arrays de chars se comienza a contar desde el cero.
+
 /*
     Sirve para comparar dos variables strings e indicar si son iguales o no.
 */
@@ -13,3 +19,23 @@ bool comparar_variables(string primera_variable, string segunda_variable);
 */
 bool comparar_variables(const char* primera_variable, const char*segunda_variable, int index);
 
+
+/*
+    Sirve para comparar dos variables const char* e indicar si son iguales o no.
+    Realiza la comparación en tiempo de compilacion. 
+    Es una modificacion de la funcion de arriba, la cual elegi en el inciso 4a, para
+    realizar el inciso 4c.
+*/
+constexpr bool comparar_variables_compilacion(const char* primera_variable, const char* segunda_variable, int index){
+    //Primer caso base: ambos arrays estan vacios.
+    if((primera_variable[index] == '\0') && (segunda_variable[index] == '\0')){
+        return true;
+    }
+    //Comparo los chars apartados anteriormente:
+    if(!(primera_variable[index] == segunda_variable[index])){ 
+        return false;
+    }
+    /*Si son iguales, aumento el valor de index en una unidad, para poder comparar el siguiente char en el array, cuando 
+    realice la recursividad.*/
+    return comparar_variables_compilacion(primera_variable, segunda_variable, index + 1);
+}
--- Ejercicio 4/main_ejercicio_4_comparando_variables.cpp
+++ Ejercicio 4/main_ejercicio_4_comparando_variables.cpp
@@ -1,6 +1,7 @@
 #include "ejercicio_4_comparando_variables.hpp"
 
 int main(){
+    //Incisos 4a y 4b:
     //Mando a correr mi funcion que compara dos variables string, para ver cuanto tiempo tarda:
     //Declaro y defino las variables que voy a comparar (eleccion arbitraria):
     string primera_variable = "Se requiere el código de una función recursiva que compare dos variables que contengan texto e indique mediante una variable bool si son iguales.";
@@ -48,7 +49,30 @@ int main(){
     el uso de .length() puede aportar un poco a esa diferencia en nanosegundos. Por ultimo, considero tambien que la diferencia 
     se puede dar debido a que const char* es un puntero, por lo que es mucho mas rapido acceder a los valores guardados en memoria.*/
 
-
+    //Inciso 4c:
+    //Uso el codigo proporcionado por la consigna para poder calcular el tiempo de ejecucion de mi funcion:
+    auto startTime3 = std::chrono::high_resolution_clock::now();
+    bool resultado_char2 = comparar_variables_compilacion(variable1, variable2, index_2);
+    auto endTime3 = std::chrono::high_resolution_clock::now();
+    auto elapsedTime3 = std::chrono::duration_cast<std::chrono::nanoseconds>(
+    endTime3 - startTime3);
+    std::cout << "A comparar_variables le tomó: " << elapsedTime3.count() << " nanosegundos" << std::endl;
+    //Utilizo un condicional para mostrar en la terminal si mis dos variables son o no iguales:
+    if(resultado_char2){
+        cout << "Son iguales las variables" << endl;
+    }
+    else{
+        cout << "No son iguales" << endl;
+    }
+    /* Reflexion acerca de la diferencia entre el tiempo de ejecucion de mi funcion 4a y esta:
+    Considero que el tiempo de ejecucion de mi funcion 4c es menor a la del punto 4a ya que, en este inciso, estoy usando
+    el calificador "constexpr" en la declaracion de la funcion. Este calificador se utiliza para indicar que se define una 
+    constante, la cual puede ser evaluada en tiempo de compilacion. Es decir, al utilizarlo, es el compilador quien decide 
+    de realizar la evaluacion en tiempo de compilacion. Para lograr esto, defini en mi archivo .hpp los parametros de entrada 
+    de la funcion (variable1[], variable2[] y index_2) con "constexpr", para que sus valores puedan ser obtenidos en tiempo
+    de compilacion. En resumen, al usar este calificador, el compilador ejecuta la funcion en tiempo de compilacion, lo que
+    vuelve el codigo mas rapido ya que el resultado va a estar precalculado para cuando el progrma se ejecute. Entonces, cuando
+    se invoca la funcion en mi main, el compilador directamnete reemplaza la llamada por el valor precalculado.*/
 
     return 0;
 }
